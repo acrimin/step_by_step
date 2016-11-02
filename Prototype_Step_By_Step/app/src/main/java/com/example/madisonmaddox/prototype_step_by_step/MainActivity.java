2b8f9a286d0eb2fd7a37312dc96b1b25dabbb775
acrimin <acrimin@clemson.edu>
2016-11-01 21:16:18 -0400

    files

---
+++ Prototype_Step_By_Step/app/src/main/java/com/example/madisonmaddox/prototype_step_by_step/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.madisonmaddox.prototype_step_by_step;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button but1;
+
+    public void changeActivity(View v) {
+        startActivity(new Intent(this, SuggestedTasks.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
