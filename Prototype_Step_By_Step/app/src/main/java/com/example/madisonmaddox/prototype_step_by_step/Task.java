82269f2c686abe8eaaaefd6d3f1a4c7abf8a084f
Madison Maddox <madisonmaddox@162.230.21.198.tigernet.wifi.dyn.clemson.edu>
2016-11-28 20:28:32 -0500

    Made Task.java more organized

--- Prototype_Step_By_Step/app/src/main/java/com/example/madisonmaddox/prototype_step_by_step/Task.java
+++ Prototype_Step_By_Step/app/src/main/java/com/example/madisonmaddox/prototype_step_by_step/Task.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Task extends AppCompatActivity {
 
@@ -15,6 +16,7 @@ public class Task extends AppCompatActivity {
 
         Button goSuggested = (Button) findViewById(R.id.button6);
         Button goAll = (Button) findViewById(R.id.button7);
+        ImageButton goStepByStep = (ImageButton) findViewById(R.id.imageButton);
 
         goSuggested.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
@@ -26,6 +28,13 @@ public class Task extends AppCompatActivity {
         goAll.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 Intent nextScreen = new Intent(getApplicationContext(), AllTasks.class);
+                startActivity(nextScreen);
+            }
+        });
+
+        goStepByStep.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent nextScreen = new Intent(getApplicationContext(), MiddleStep.class);
                 startActivity(nextScreen);
             }
         });
