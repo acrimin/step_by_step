4245a5c652441ea9a71c5d88671b18f3c50a8871
Madison Maddox <madisonmmaddox@gmail.com>
2016-11-29 16:02:38 -0500

    TaskPage now has video button that opens youtube URL in browser

--- Prototype_Step_By_Step/app/src/main/java/com/example/madisonmaddox/prototype_step_by_step/TaskPage.java
+++ Prototype_Step_By_Step/app/src/main/java/com/example/madisonmaddox/prototype_step_by_step/TaskPage.java
@@ -1,8 +1,10 @@
 package com.example.madisonmaddox.prototype_step_by_step;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,6 +19,7 @@ public class TaskPage extends AppCompatActivity {
         Button goSuggested = (Button) findViewById(R.id.button6);
         Button goAll = (Button) findViewById(R.id.button7);
         ImageButton goStepByStep = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton goVideo = (ImageButton) findViewById(R.id.videoBtn);
 
         goSuggested.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
@@ -36,6 +39,13 @@ public class TaskPage extends AppCompatActivity {
             public void onClick(View arg0) {
                 Intent nextScreen = new Intent(getApplicationContext(), MiddleStep.class);
                 startActivity(nextScreen);
+            }
+        });
+
+        goVideo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=aOKFUdPEYg4")));
+                Log.i("Video", "Video Playing....");
             }
         });
     }
